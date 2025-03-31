5700b34ec83fc724972f644750320c4e8eba381a
JChain35 <chain33335555@gmail.com>
2025-03-31 15:32:48 +0800

    feat(rtl8152): update header for rtl8153

--- class/vendor/net/usbh_rtl8152.h
+++ class/vendor/net/usbh_rtl8152.h
@@ -25,6 +25,26 @@ struct usbh_rtl8152 {
     uint8_t eee_adv;
     uint8_t eee_en;
     uint8_t supports_gmii;
+
+    unsigned long flags;
+    struct ups_info {
+        uint32_t r_tune          : 1;
+        uint32_t _10m_ckdiv      : 1;
+        uint32_t _250m_ckdiv     : 1;
+        uint32_t aldps           : 1;
+        uint32_t lite_mode       : 2;
+        uint32_t speed_duplex    : 4;
+        uint32_t eee             : 1;
+        uint32_t eee_lite        : 1;
+        uint32_t eee_ckdiv       : 1;
+        uint32_t eee_plloff_100  : 1;
+        uint32_t eee_plloff_giga : 1;
+        uint32_t eee_cmod_lv     : 1;
+        uint32_t green           : 1;
+        uint32_t flow_control    : 1;
+        uint32_t ctap_short_off  : 1;
+    } ups_info;
+
     uint16_t min_mtu;
     uint16_t max_mtu;
     uint16_t ocp_base;
@@ -32,10 +52,10 @@ struct usbh_rtl8152 {
     uint32_t rx_buf_sz;
 
     struct rtl_ops {
-        void (*init)(struct usbh_rtl8152 *tp);
+        int (*init)(struct usbh_rtl8152 *tp);
         int (*enable)(struct usbh_rtl8152 *tp);
         void (*disable)(struct usbh_rtl8152 *tp);
-        void (*up)(struct usbh_rtl8152 *tp);
+        int (*up)(struct usbh_rtl8152 *tp);
         void (*down)(struct usbh_rtl8152 *tp);
         void (*unload)(struct usbh_rtl8152 *tp);
         bool (*in_nway)(struct usbh_rtl8152 *tp);
@@ -59,6 +79,9 @@ void usbh_rtl8152_stop(struct usbh_rtl8152 *rtl8152_class);
 uint8_t *usbh_rtl8152_get_eth_txbuf(void);
 int usbh_rtl8152_eth_output(uint32_t buflen);
 void usbh_rtl8152_eth_input(uint8_t *buf, uint32_t buflen);
+
+int usbh_rtl8152_eth_tx(uint8_t *buf, uint32_t buflen);
+
 void usbh_rtl8152_rx_thread(CONFIG_USB_OSAL_THREAD_SET_ARGV);
 
 #ifdef __cplusplus
